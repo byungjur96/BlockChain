45225eba9856108545c2d546d88251c8ec70f81e
Byungju Chae <cbyungjub@naver.com>
2018-10-12 00:40:29 +0900

    RSA p, q 임의 범위로 설정 가능하게 변경 자동 반복 가능

--- cryptosystem/RSA.py
+++ cryptosystem/RSA.py
@@ -6,6 +6,20 @@ import sys
 
 
 class Euclide:
+    # 범위 내의 소수를 모두 구하는 함수
+    @staticmethod
+    def find_prime(variable):
+        result = set({})
+        a, b = map(int, input('Define range of '+variable+':').split(','))
+        for n in range(a, b):
+            state = True
+            for i in range(2, n):
+                if n % i == 0:
+                    state = False
+            if state:
+                result.add(n)
+        return result
+
     # 최대공약수를 구하는 함수
     @staticmethod
     def gcd(a, b):
@@ -26,28 +40,20 @@ class Euclide:
 class RSA:
     # 7,000 ~ 8,000 사이의 임의의 소수를 출력하는 함수
     def make_p(self):
-        list_p = set({})
-        for n in range(7000, 8000):
-            state = True
-            for i in range(2, n):
-                if n % i == 0:
-                    state = False
-            if state:
-                list_p.add(n)
+        list_p = Euclide.find_prime('p')
+        while len(list_p) == 0:
+            print('Chosen range has no prime number!')
+            list_p = Euclide.find_prime('p')
         list_p = list(list_p)
         p = list_p[random.randrange(0, len(list_p))]
         return p
 
     # 9,000 ~ 10,000 사이의 임의의 소수를 출력하는 함수
     def make_q(self):
-        list_q = set({})
-        for n in range(9000, 10000):
-            state = True
-            for i in range(2, n):
-                if n % i == 0:
-                    state = False
-            if state:
-                list_q.add(n)
+        list_q = Euclide.find_prime('q')
+        while len(list_q) == 0:
+            print('Chosen range has no prime number!')
+            list_q = Euclide.find_prime('q')
         list_q = list(list_q)
         q = list_q[random.randrange(0, len(list_q))]
         return q
@@ -96,7 +102,13 @@ def fast_c(a, n, z):
     return result
 
 
-def encrypt(n, e):
+def encrypt():
+    rsa = RSA()
+    p = rsa.make_p()
+    q = rsa.make_q()
+    [n, pi] = rsa.make_key(p, q)
+    e = rsa.find_e(pi)
+    d = rsa.find_d(e, pi)
     mesg = input('Write a message to Encrypt: ')
     log = "Encryping '" + mesg + "'"
     print_mesg(log)
@@ -106,14 +118,24 @@ def encrypt(n, e):
     for m in code:
         c = fast_c(m, e, n)
         crypto.append(c)
+    print('Public key: ' + str([e, n]))
+    print('Private key: ' + str([d, n]))
     print('Encrypted message is', crypto)
+    print()
 
 
-def decrypt(n, d):
+def decrypt():
+    n1 = None
+    n2 = None
+    while n1 is None or n2 is None or (n1 != n2):
+        e, n1 = map(int, input('Enter a Public Key: ').split(','))
+        d, n2 = map(int, input('Enter a Private Key: ').split(','))
+
     pw = list(map(int, input('Write a message to Decrypt: ').split(',')))
-    mesg = ''.join([chr(fast_c(c, d, n)) for c in pw])
+    mesg = ''.join([chr(fast_c(c, d, n1)) for c in pw])
     print_mesg('Decrypting')
     print("The message was '" + mesg + "'")
+    print()
 
 
 def print_mesg(msg):
@@ -126,25 +148,21 @@ def print_mesg(msg):
 
 
 def main():
-    p = 7151
-    q = 9587
-    n = 68556637
-    pi = 68539900
-    e = 21110703
-    d = 38951467
-
-    print("<Option>")
-    print("1. Encrypt")
-    print("2. Decrypt")
-
-    option = input('Enter Option: ')
-    if option == '1':
-        encrypt(n, e)
-    elif option == '2':
-        decrypt(n, d)
-    else:
-        print('Wrong Option! Try again')
-        main()
+    option = -1
+    while option != '0':
+        print("<Option>")
+        print("1. Encrypt")
+        print("2. Decrypt")
+        print("Enter '0' to exit")
+        option = input('Enter Option: ')
+        if option == '1':
+            encrypt()
+        elif option == '2':
+            decrypt()
+        elif option == '0':
+            print('End this Program')
+        else:
+            print('Wrong Option! Try again')
 
 
 if __name__ == '__main__':
